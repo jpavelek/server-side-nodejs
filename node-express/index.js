1e7112a860c557c7c7bb8f66c9f3c7ba43cee204
Jakub Pavelek <jpavelek@live.com>
2018-01-19 17:31:11 +0100

    Add morgan and serve static pages

--- node-express/index.js
+++ node-express/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
-const http = require("http")
+const http = require("http");
+const morgan = require("morgan");
+
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
+app.use(morgan("dev"));
+
+//Serve static webpages from location relateive to current working dir
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, resp, next) => {
-    console.log(req.headers);
     resp.status = 200;
     resp.setHeader("Content-Tyle", "text/html");
     resp.end("<html><body><h1>Express server</h1></body></html>")
