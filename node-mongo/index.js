b1e9f98adf490df19b4abd34e995d2cec2e96b82
Jakub Pavelek <jpavelek@live.com>
2018-01-23 09:35:30 +0100

    Implement simplie node mongodb client.

---
+++ node-mongo/index.js
@@ -0,0 +1,33 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+const url = "mongodb://localhost:27017";
+
+MongoClient.connect(url, (err, dbs) => {
+    assert.equal(err, null);
+
+    console.log("Connected to the server");
+    const db = dbs.db("conFusion");
+
+    const collection = db.collection("dishes");
+
+    collection.insertOne({"name":"Hotdog", "description":"Typical ..."}, (err, result) => {
+        assert.equal(err, null);
+
+        console.log("After insert:\n");
+        console.log(result.ops);
+
+        collection.find({}).toArray((err, docs) => {
+            assert.equal(err, null);
+
+            console.log("Found:\n");
+            console.log(docs);
+
+            db.dropCollection("dishes", (err, result) => {
+                assert.equal(err, null);
+
+                dbs.close();
+            });
+        });
+    });
+});
