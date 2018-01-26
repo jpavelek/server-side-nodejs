f702f5e45e10d8db7d523546ba77f45726217f33
Jakub Pavelek <jpavelek@live.com>
2018-01-26 13:33:59 +0100

    Implement REST API for /ishes end-point. Mongo, mongoose and express.

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -11,6 +11,19 @@ var dishRouter = require('./routes/dishRouter');
 var promotRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+mongoose.Promise = require("bluebird");
+
+const Dishes = require("./models/dishes");
+const url = "mongodb://localhost:27017/conFusion";
+const connect = mongoose.connect(url, { userMongoClient: true});
+
+connect.then(function(db) {
+  console.log("Connected to server");
+}, function(err) {
+  console.log(err);
+})
+
 var app = express();
 
 // view engine setup
--- conFusionServer/routes/dishRouter.js
+++ conFusionServer/routes/dishRouter.js
@@ -1,47 +1,106 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const Dishes = require("../models/dishes");
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
 //Router for general /dishes
 dishRouter.route("/")
-.all((req, resp, next) => {
-    resp.statusCode = 200;
-    resp.setHeader("Content-Type", 'text/plain');
-    next();
-})
 .get((req, resp, next) => {
-    resp.end("Will send all the dishes later");
+    Dishes.find({})
+    .then(function(dishes) {
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+        resp.json(dishes);
+    }, function(err) {
+        next(err);
+    })
+    .catch(function(err) {
+        next(err);
+    })
 })
 .post((req, resp, next) => {
-    resp.end("Will add dish: " + req.body.name + " with data " + req.body.description);
+    Dishes.create(req.body)
+    .then(function(dish) {
+        console.log("Dish created ", dish);
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+        resp.json(dish);
+    }, function(err) {
+        next(err);
+    })
+    .catch( function(err) {
+        next(err);
+    })
 })
 .put((req, resp, next) => {
     resp.statusCode = 403;
     resp.end("PUT operation not supported");
 })
 .delete((req, resp, next) => {
-    resp.end("Delete all the dishes!");
+    Dishes.remove({})
+    .then( function(res) {
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+        resp.json(res);
+    }, function(err) {
+        next(err);
+    })
+    .catch( function(err) {
+        next(err);
+    })
 });
 
 
 //Router for /dishes/:dishId
 dishRouter.route("/:dishId")
 .get((req, resp, next) => {
-    resp.end("Will send details of " + req.params.dishId);
+    Dishes.findById(req.params.dishId)
+    .then(function(dish) {
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+        resp.json(dish);
+    }, function(err) {
+        next(err);
+    })
+    .catch( function(err) {
+        next(err);
+    })
 })
 .post((req, resp, next) => {
     resp.statusCode = 403;
     resp.end("POST operation not supported on /dishes/" + req.params.dishId);
 })
 .put((req, resp, next) => {
-    resp.write("Updating dish " + req.params.dishId + "\n");
-    resp.end("Will update dish " + req.body.name + " with description: " + req.body.description);
+    Dishes.findByIdAndUpdate(req.params.dishId, {
+        $set: req.body
+    }, { new: true })
+    .then(function(dish) {
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+        resp.json(dish);
+    }, function(err) {
+        next(err);
+    })
+    .catch( function(err) {
+        next(err);
+    })
 })
 .delete((req, resp, next) => {
-    resp.end("Deleting dish " + req.body.name);
+    Dishes.findByIdAndRemove(req.params.dishId)
+    .then( function(res) {
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+        resp.json(res);
+    }, function(err) {
+        next(err);
+    })
+    .catch( function(err) {
+        next(err);
+    })
 });
 
 module.exports = dishRouter;
